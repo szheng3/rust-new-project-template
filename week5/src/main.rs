97d256f0a71e56fd3cf4065121b7c7348d3d6a33
Sept <sszheng3@gmail.com>
2023-02-21 23:02:10 -0500

    added one more endpoints

--- week5/src/main.rs
+++ week5/src/main.rs
@@ -1,10 +1,32 @@
 use actix_files ::Files;
-use actix_web::{App, HttpServer};
+use actix_web::{get, App, HttpResponse, HttpServer, Responder, web};
+
+use serde::Serialize;
+use serde::Deserialize;
+#[derive(Serialize)]
+pub struct GenericResponse {
+    pub status: String,
+    pub message: String,
+}
+
+
+#[get("/api/health")]
+async fn api_health_handler() -> HttpResponse {
+    let response_json = &GenericResponse {
+        status: "success".to_string(),
+        message: "Health Check".to_string(),
+    };
+    HttpResponse::Ok().json(response_json)
+}
+
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new().service(
+        App::new()
+            .service(api_health_handler)
+            .service(
+
             Files::new("/", "./dist").index_file("index.html")
             ,
         )
